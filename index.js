48aa30bb0b3c8fa5ffc60ad3b327dc6ca146ba9b
arunsharmaksolves <161150662+arunsharmaksolves@users.noreply.github.com>
2024-03-20 11:37:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // Function to add event listener for specified elements
-    window.addEventListenerForElements = function(elementsToTrack, eventType) {
+    export function addEventListenerForElements(elementsToTrack, eventType) {
     elementsToTrack.forEach(elementCriteria => {
         const elements = document.querySelectorAll(elementCriteria);
         elements.forEach(element => {
@@ -41,7 +41,8 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     });
-};
+}
+
 
     // // Example usage
     // const buttonsToTrack = ['#addToCart', '#like']; // Client-specified button IDs
