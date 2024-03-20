50f1589f8c8b2f7997fc2c8977d32d1514fcb9e2
arunsharmaksolves <161150662+arunsharmaksolves@users.noreply.github.com>
2024-03-20 16:34:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-console.log("File added");
+addEventListenerForElements = function(elementsToTrack, eventType) {
+    elementsToTrack.forEach(elementCriteria => {
+        const elements = document.querySelectorAll(elementCriteria);
+        elements.forEach(element => {
+            element.addEventListener(eventType, (event) => {
+                if (eventType === 'submit') {
+                    event.preventDefault(); // Prevent default form submission behavior
+                }
+                const eventData = {
+                    event: eventType,
+                    timestamp: new Date().toISOString(),
+                    target: {
+                        tagName: event.target.tagName,
+                        id: event.target.id,
+                        innerText: event.target.innerText
+                    }
+                };
 
-function printHelloWorld(){
- console.log("Hello World");
-}
+                window.dataLayer = window.dataLayer || [];
+                window.dataLayer.push(eventData);
+                console.log(`${eventType} event fired on ${elementCriteria} at`, new Date());
+            });
+        });
+    });
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("DOM Loaded");
+
+    // Function to check if an element matches the specified criteria
+    function isElementToTrack(element, criteria) {
+        return (
+            element.tagName.toLowerCase() === criteria.toLowerCase() || // Check tag name
+            element.id === criteria // Check HTML tag ID
+        );
+    }
+});
